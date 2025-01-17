f9df905bc5f775a0e4f3376d7105c09f0eecf6ea
gufronnakaaw <gufronnakaaw@gmail.com>
2025-01-17 22:01:55 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
+import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './utils/global/global.exeption';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix('api', {
+    exclude: ['/'],
+  });
+
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
+  app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
+
+  await app.listen(process.env.MODE === 'prod' ? 4002 : 4003);
+}
+bootstrap();
---
+++ src/utils/global/global.response.ts
@@ -0,0 +1,6 @@
+export type SuccessResponse = {
+  success: boolean;
+  status_code: number;
+  message?: string;
+  data?: unknown;
+};
