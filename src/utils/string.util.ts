f762386b277d37e9d7ec3fa72071cd73989b1093
gufronnakaaw <gufronnakaaw@gmail.com>
2025-01-21 23:25:32 +0700

    create: text & bcrypt utility

---
+++ src/utils/string.util.ts
@@ -0,0 +1,21 @@
+export function getReadingTimeFromHTML(html: string) {
+  const wordsPerMinute = 200;
+  const plainText = html.replace(/<\/?[^>]+(>|$)/g, '');
+  const words = plainText.trim().split(/\s+/).length;
+  const minutes = Math.ceil(words / wordsPerMinute);
+  return `${minutes} menit waktu baca`;
+}
+
+export function getInitials(words: string) {
+  return words
+    .split(' ')
+    .map((word) => word.charAt(0))
+    .join('');
+}
+
+export function capitalize(words: string) {
+  return words
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
