3d32877bfc76072e33b2412f9190bc6a0e7abe15
gufronnakaaw <gufronnakaaw@gmail.com>
2025-01-24 20:25:48 +0700

    add: new constanta

--- src/utils/constant.util.ts
+++ src/utils/constant.util.ts
@@ -9,3 +9,25 @@ export const PREFIX = {
   EVENT: 'JPSEVT',
   ADMIN: 'JPSADM',
 };
+
+export const EDUCATIONORDER: Record<string, number> = {
+  TK: 1,
+  SD: 2,
+  SMP: 3,
+  SMA: 4,
+  SMK: 5,
+  D1: 6,
+  D2: 7,
+  D3: 8,
+  D4: 9,
+  S1: 10,
+  S2: 11,
+  S3: 12,
+};
+
+export const SOCIALMEDIAORDER: Record<string, number> = {
+  TIKTOK: 1,
+  YOUTUBE: 2,
+  INSTAGRAM: 3,
+  LINKEDIN: 4,
+};
