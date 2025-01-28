b1f56ca9c3f3f65b19e223315db7f4b480e9a5a9
gufronnakaaw <gufronnakaaw@gmail.com>
2025-01-28 13:34:02 +0700

    update: guard utility

--- src/utils/guards/public.guard.ts
+++ src/utils/guards/public.guard.ts
@@ -8,19 +8,25 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
 @Injectable()
-export class SuperAdminGuard implements CanActivate {
+export class PublicGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest() as Request;
     const token = this.extractToken(request);
+    const role = request.headers['x-role'];
+
+    if (role !== 'admin') {
+      return true;
+    }
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET_KEY,
       });
 
-      if (payload.role == 'superadmin') {
+      if (payload.role == 'admin' || payload.role == 'superadmin') {
+        request['admin'] = payload;
         return true;
       }
 
