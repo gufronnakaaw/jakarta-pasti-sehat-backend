560e23dc9172a79c33a931c40a9d0ca3efa1c974
gufronnakaaw <gufronnakaaw@gmail.com>
2025-02-12 12:48:10 +0700

    add: access_key at admin login

--- src/app.dto.ts
+++ src/app.dto.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const adminLoginSchema = z.object({
+  access_key: z.string().min(1, { message: 'Admin ID wajib diisi' }),
   admin_id: z.string().min(1, { message: 'Admin ID wajib diisi' }),
   password: z.string().min(8, { message: 'Password minimal 8 karakter' }),
 });
